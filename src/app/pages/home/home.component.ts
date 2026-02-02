a2ba60a6fee34b1b61da50835fea2d2dbd32006c
Sameer Singh <52269967+CodeInError@users.noreply.github.com>
2026-02-02 10:25:21 +0000

    Complete Design for Home Page

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-home',
+  imports: [CardModule,ButtonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
